3524f7d6c4cbe8abeffe9c7e7401f8c49a53f159
nnstt1 <nnstt1@gmail.com>
2024-01-31 16:05:42 +0900

    Add VNet

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "example-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
